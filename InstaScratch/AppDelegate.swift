1607d825a78c3d77b61ff8f4625662d8b09ca82b
Dylan James SMith <dsmith95@mail.ccsf.edu>
2016-03-01 20:56:45 -0800

    init Parse

--- InstaScratch/AppDelegate.swift
+++ InstaScratch/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // Initialize Parse
+        Parse.initializeWithConfiguration(
+            ParseClientConfiguration(block: { (configuration:ParseMutableClientConfiguration) -> Void in
+                configuration.applicationId = "InstaScratch"
+                configuration.server = "https://instascratch.herokuapp.com/parse"
+            })
+        )
+        
         return true
     }
 
